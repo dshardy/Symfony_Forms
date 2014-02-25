98a5c2b8edd94298ef8b2a768700ea64a0550eb1
Darren Hardy <darren_hardy@icloud.com>
2014-02-25 16:09:37 +0000

    Back to hello/peter

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
            
             new Dsh\NewformBundle\DshNewformBundle(),
-            new Dsh\TaskBundle\DshTaskBundle(),
+            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Dsh/TaskBundle/DshTaskBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dsh\TaskBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DshTaskBundle extends Bundle
-{
-}
